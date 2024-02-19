702c1d0f84a4b965fb6ae658a15c82fe18579b1b
d1sgu1sed <ivan.kichigin977@gmail.com>
2024-02-19 14:46:21 +0000

    Added finding of files

--- FindFiles.sh
+++ FindFiles.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
 if [[ "$#" -ne 2 ]]; then
-    echo "Use: $0 <dir> <min_len>"
-    exit -1
+	echo "Use: $0 <dir> <min_len>"
+    	exit -1
 fi
 
 dir="$1"
 min_len="$2"
 
-echo $dir $min_len
+find "$dir" -type f | while read -r file; do
+    	filename=$(basename "$file")
+	filename="${filename%.*}"
+    	if [[ ${#filename} -ge $min_len ]]; then
+        	echo "$file"
+    	fi
+done | sort > list_files.txt
+
+echo "Saved to list_files.txt"
